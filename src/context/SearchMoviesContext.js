57b9ff89c69a90f13f88bc60fe8c9a19e3b02f11
sethu786 <sethudasari77@gmail.com>
2024-12-31 05:48:33 +0000

    commit message

---
+++ src/context/SearchMoviesContext.js
@@ -0,0 +1,9 @@
+
+import {createContext} from 'react'
+
+const SearchMoviesContext = createContext({
+  searchResponse: {},
+  onTriggerSearchingQuery: () => {},
+})
+
+export default SearchMoviesContext
